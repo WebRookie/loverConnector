e517e93675f0f3824cc452975b459b9e2f7fbfb2
WebRookie <912962951@qq.com>
2023-07-12 00:35:01 +0800

    program create

---
+++ loverConnector/src/main/java/com/rookie/loverconnector/LoverConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.loverconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoverConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoverConnectorApplication.class, args);
+    }
+
+}
---
+++ loverConnector/src/test/java/com/rookie/loverconnector/LoverConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.loverconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoverConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
